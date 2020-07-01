d06f7ab427c6cc0f608304b30e5109003a5ab856
Allen Kreager <akreager87@gmail.com>
2020-07-01 13:53:55 -0400

    Incresse max XY feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -751,7 +751,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 250, 250, 35, 22}
+#define DEFAULT_MAX_FEEDRATE          { 360, 360, 35, 22}
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
