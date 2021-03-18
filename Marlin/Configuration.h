3769648b1f27d2f4894a3a8863921dabdd151c53
Allen Kreager <akreager87@gmail.com>
2021-03-18 11:55:36 -0400

    Update extruder PID K values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -509,9 +509,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  18.33
+    #define DEFAULT_Ki   1.55
+    #define DEFAULT_Kd  54.21
   #endif
 #endif // PIDTEMP
 
