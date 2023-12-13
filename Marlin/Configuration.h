656396217bb37926caa91b6097540c0cd74a3321
Allen Kreager <akreager87@gmail.com>
2023-12-12 22:00:51 -0500

    increase X&Y bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1694,8 +1694,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 125
-#define Y_BED_SIZE 159
+#define X_BED_SIZE 126
+#define Y_BED_SIZE 177
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
