d3a0bae66bfc4250c3fd3d6b220d267a8696dceb
Allen Kreager <akreager87@gmail.com>
2023-12-13 20:27:15 -0500

    reduce bed length

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1695,7 +1695,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 126
-#define Y_BED_SIZE 177
+#define Y_BED_SIZE 176
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
