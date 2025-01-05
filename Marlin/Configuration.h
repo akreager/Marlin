ea5c15121d27a642f44f5928d145eafa2274ec6b
Allen Kreager <akreager87@gmail.com>
2025-01-05 13:48:24 -0500

    update hotend PID terms

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -667,9 +667,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 35.68
-    #define DEFAULT_Ki 3.20
-    #define DEFAULT_Kd 99.56
+    #define DEFAULT_Kp 40.48
+    #define DEFAULT_Ki 3.56
+    #define DEFAULT_Kd 115.03
   #endif
 #endif
 
